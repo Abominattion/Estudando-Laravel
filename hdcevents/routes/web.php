ea743b23400622de2dfeca488dd54ee5be47567e
Adonay Douglas <adonaypowermetal@gmail.com>
2022-02-24 14:44:02 -0300

    Rotas e Views

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contato', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('products');
+});
+
